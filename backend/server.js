a6b4f34e93873c646745715c1f68d1d61d68976f
Varalakshmi Pokala <varalakshmipokala7@gmail.com>
2026-01-25 16:09:32 +0530

    serve frontend properly

--- backend/server.js
+++ backend/server.js
@@ -131,8 +131,10 @@ app.listen(PORT, () =>
 );
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+app.get("/", (req,res)=>{
+   res.sendFile(__dirname + "/public/index.html");
+});
+
 });
 
 
